ea98e8a84a44b8bd1d032b8c56cff4cd5b0a1c70
kalebesampaio <kalebesmp2002@gmail.com>
2024-02-20 14:06:54 -0300

    feat: criando estrutura base do projeto

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,92 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+
+//Reset Css
+
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+button{
+    cursor: pointer;
+}
+
+//Colors
+
+
+:root{
+//Brand
+    --brand1: #4529E6;
+    --brand2: #5126ea;
+    --brand3: #B0A6F0;
+    --brand4: #edeafd;
+
+//Grey Scale
+    --grey0: #0b0d0d;   
+    --grey1: #212529;
+    --grey2: #495057;
+    --grey3: #868E96;
+    --grey4: #ADB5BD;
+    --grey5: #CED4DA;
+    --grey6: #DEE2E6;
+    --grey7: #E9ECEF;
+    --grey8: #F1F3F5;
+    --grey9: #F8F9FA;
+    --grey10: #FDFDFD;
+    --whiteFixed: #FFFFFF;
+
+//FeedBack
+    --alert1: #CD2B31;
+    --alert2: #FDD8D8;
+    --alert3: #FFE5E5;
+    --sucess1: #18794E;
+    --sucess2: #CCEBD7;
+    --sucess3: #DDF3E4;
+}
+`;
