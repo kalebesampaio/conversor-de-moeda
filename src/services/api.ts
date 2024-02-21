0c72bcf7deba73588045ddae19c4cc35a9a9e5c7
kalebesampaio <kalebesmp2002@gmail.com>
2024-02-21 20:59:29 -0300

    fix: configurando o https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://economia.awesomeapi.com.br/",
+  baseURL: "https://economia.awesomeapi.com.br/",
   timeout: 5 * 1000,
 });
